f397b8713931ce09651686ddfd97eb0b33e28065
Kyle Allain <39541671+Raccoonn@users.noreply.github.com>
2020-02-19 16:20:54 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from Plot_BeverageCounts import Plot_BeverageCounts
 if __name__ == '__main__':
     '''
     Command line interface to produce clean counter logs and plot if desired.
-
-    Tips: - Make sure your input/output filenames include '5000S+' or '9000F'
-            The Parser will filter out certain lines based on which machine the 
-            log file is from.
     '''
 
     print('\n\n5000/9000 Beverage count parser.\n')
