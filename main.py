31de7ea4d269f76d2cceacce38b168cb39be250c
Kyle Allain <39541671+Raccoonn@users.noreply.github.com>
2020-02-26 06:59:51 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Parser_BeverageCounts import Parse_BeverageCounts
 from Plot_BeverageCounts import Plot_BeverageCounts
+from Volume_BeverageCounts import Machine_WaterVolumes
 
 if __name__ == '__main__':
     '''
@@ -11,11 +12,31 @@ if __name__ == '__main__':
     '''
 
     print('\n\n5000/9000 Beverage count parser.\n')
-    filename_rawInput = input('\nInput raw data filename:  ')
-    filename_cleanOutput = input('\nInput output filename:  ')
+    Input_5000 = input('\nInput 5000S+ raw data filename:  ')
+    Output_5000 = input('\nInput 5000S+ output filename:  ')
 
-    Parse_BeverageCounts(filename_rawInput, filename_cleanOutput)
+    Input_9000 = input('\n\nInput 9000F raw data filename:  ')
+    Output_9000 = input('\nInput 9000F output filename:  ')
 
-    plot_data = input('\nWould you like to plot the data? (Y/N): ')
-    if plot_data not in ['N', 'n', 'No', 'no']:
-        Plot_BeverageCounts(filename_cleanOutput)
+    input('\n\nPress Enter to process 5000S+ data.\n\n')
+
+    Parse_BeverageCounts(Input_5000, Output_5000)
+
+    plot_data = input('Would you like to plot the data? (Y/N): ')
+    if plot_data in ['Y', 'y', 'Yes', 'yes']:
+        Plot_BeverageCounts(Output_5000)
+
+
+    input('\n\nPress Enter to process 9000F data.\n\n')
+
+    Parse_BeverageCounts(Input_9000, Output_9000)
+
+    plot_data = input('Would you like to plot the data? (Y/N): ')
+    if plot_data in ['Y', 'y', 'Yes', 'yes']:
+        Plot_BeverageCounts(Output_9000)   
+
+
+    calc_volumes = input('\n\nWould you like to estimate water usage? (Y/N): ')
+    if calc_volumes in ['Y', 'y', 'Yes', 'yes']:
+        DaysOfOperation = input('\nInput number of days in operation: ')
+        Machine_WaterVolumes(Output_5000, Output_9000, DaysOfOperation)
